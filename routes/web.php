d476489be26d395257d2a82a3a680c8619d0f1bd
zahra <echo.zahrafelisia01@gmail.com>
2024-09-12 10:04:15 +0700

    menyusun struktur layout

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
 Auth::routes([
     'register' => false,
